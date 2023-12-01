e1eb7e1dcb007794a5abc6af49de3b2383aa9e94
William DeDominico <72836413+Moriz82@users.noreply.github.com>
2023-11-30 23:48:01 -0600

    Update speaker.lua

--- speaker.lua
+++ speaker.lua
@@ -1,12 +1,11 @@
+local dfpwm = require("cc.audio.dfpwm")
 local speaker = peripheral.find("speaker")
-local file = fs.open("my_file.dfpwm", "rb")
 
-while true do
-  local bytes = file.read(16384)
-  if not bytes then return end
-  speaker.playAudio(bytes)
+local decoder = dfpwm.make_decoder()
+for chunk in io.lines("data/example.dfpwm", 16 * 1024) do
+    local buffer = decoder(chunk)
 
-  -- Alternatively, make playAudio block until its buffer is empty, meaning
-  -- we can drop this line.
-  repeat local _, name = os.pullEvent("need_audio") until name == peripheral.getName(speaker)
+    while not speaker.playAudio(buffer) do
+        os.pullEvent("speaker_audio_empty")
+    end
 end
